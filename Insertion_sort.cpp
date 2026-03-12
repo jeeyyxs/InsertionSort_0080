6096f5f7e2f610e13c77d85b6a8e195b239dede1
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-03-12 18:20:21 +0700

    3

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -1,2 +1,20 @@
 #include <iostream>
 using namespace std;
+
+int arr[20];              //membuat array dengan panjang data 20
+int n;                    //membuat variable inputan n
+
+void input()
+{
+    while (true){
+        cout << "Masukkan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray Tidak Boleh Lebih dari 20\n";
+        }
+    }
+}
