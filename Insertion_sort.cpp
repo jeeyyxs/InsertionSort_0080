93323b968d859c059d9b87a1312dc8f7759e5d04
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-03-12 18:24:29 +0700

    4

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int arr[20];              //membuat array dengan panjang data 20
 int n;                    //membuat variable inputan n
 
-void input()
+void input()//prosedur input
 {
     while (true){
         cout << "Masukkan Jumlah Data Pada Array : ";
@@ -16,5 +16,14 @@ void input()
         else{
             cout << "\nArray Tidak Boleh Lebih dari 20\n";
         }
+    }
+    cout << endl;
+    cout << "=============================================";
+    cout << "==========Masukan Elemen Array==============";
+    cout << "=============================================" << endl;
+
+    for (int i = 0; i < n; i++){
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
     }
 }
